4baaafde6a96832bd82ef0c79d4723238a69bc82
qazal <ghazal.nzhrzr@gmail.com>
2022-08-31 11:56:00 +0100

    feat(pages): darkmode + fonts + logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@fontsource/poppins";
+import "@fontsource/open-sans";
+import "@fontsource/inconsolata";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
